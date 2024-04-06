5f9ed66bb3f086280b7bcae5163366af93509e92
Roger Tang <rogertangrt@hotmail.com>
2024-04-06 08:59:16 -0300

    Aula 23

---
+++ Aula_23/PyCharm.py
@@ -0,0 +1,9 @@
+# https://www.youtube.com/watch?v=voiYTkf_W9Q&list=PLyqOvdQmGdTR46HUxDA6Ymv4DGsIjvTQ-&index=23
+# Como instalar o PyCharm em 2019 (Python para machine learning - Aula 23)
+
+# PyCharm é outra IDE que não o Vscode
+# Link ferramenta: https://www.jetbrains.com/pycharm/download/?section=windows#section=windows
+
+print('Olá Mundo!')
+a= input('Digita alguma coisa : ')
+print(a)
